750abffff93f8ce4e34d26e284e8b02619ab1e0f
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2018-06-01 15:18:02 +0300

    Added SEMAPHORE_PIPELINE_ID in hw.go

--- hw.go
+++ hw.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import ( 
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello World!")
+	fmt.Println("SEMAPHORE_PIPELINE_ID:", os.Getenv("SEMAPHORE_PIPELINE_ID"))
 }
 
